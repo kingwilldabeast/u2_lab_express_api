6d9706437d7b66133c5696d6f3decd4e6d320855
kingwilldabeast <willmur@live.com>
2024-05-26 16:03:50 -0500

    render review comments in dynamic divs onscreen

--- script.js
+++ script.js
@@ -5,15 +5,16 @@ let value2 = document.querySelector("#value2")
 let value3 = document.querySelector("#value3")
 let value4 = document.querySelector("#plot-content")
 let actorList = document.querySelector('#actor-list')
+let reviewList = document.querySelector('#review-list')
 
 button.addEventListener('click', async ()=> {
   let input = document.querySelector("#inputBar").value
-  if (input == ``) { input = 1 }
+  if (input == ``) { input = 3 }
 
   try {
     // let response = await axios.get(`http://localhost:3001/movies/${input}`);
     let response = await axios.get(`http://localhost:3001/movies`);
-    console.log(response.data); // Log the data received from the API
+    // console.log(response.data); // Log the data received from the API
     
     let singleMovie = response.data[input]
     movieName.innerText = singleMovie.title
@@ -21,14 +22,12 @@ button.addEventListener('click', async ()=> {
     value2.innerText = singleMovie.rating
     value3.innerText = singleMovie.runtimeMinutes
     value4.innerText = singleMovie.description
-
-    // let actorID = singleMovie.actors[0]
     
     let actorsIDArray = singleMovie.actors
 
     const fetchActorDetails = async (actorID) => {
       try {
-        console.log(actorID);
+        // console.log(actorID);
         let singleActor = await axios.get(`http://localhost:3001/actors/${actorID}`);
         const newElement = document.createElement('div');
         newElement.classList.add('info');
@@ -40,12 +39,41 @@ button.addEventListener('click', async ()=> {
     };
     
     const loadActors = async () => {
+      actorList.innerHTML = ''
       for (const actorID of actorsIDArray) {
         await fetchActorDetails(actorID);
       }
     };
     
     loadActors();
+
+    /////Revies
+    let reviewsIDArray = singleMovie.reviews
+    // console.log(`THE REVIEW array IS ${reviewsIDArray}`);
+
+
+    const fetchReviewDetails = async (reviewID) => {
+      try {
+        // console.log(`THE REVIEW ID IS ${reviewID}`);
+        let singleReview = await axios.get(`http://localhost:3001/reviews/${reviewID}`);
+        const newElement = document.createElement('div');
+        newElement.classList.add('info');
+        newElement.innerText = singleReview.data.comment;
+        reviewList.appendChild(newElement);
+      } catch (error) {
+        console.error(`Error fetching review with ID ${reviewID}:`, error);
+      }
+    };
+    
+    const loadReviews = async () => {
+      reviewList.innerHTML = ''
+      for (const reviewID of reviewsIDArray) {
+        await fetchReviewDetails(reviewID);
+      }
+    };
+    
+    loadReviews();
+    //reviews...
 
   } catch (error) {
     console.error('Error fetching data:', error);  
