1825f192d074a34f3fc95e8502d54189d1505d31
kingwilldabeast <willmur@live.com>
2024-05-26 15:44:29 -0500

    render actor names in dynamic divs on screen

--- script.js
+++ script.js
@@ -4,22 +4,48 @@ let value1 = document.querySelector("#value1")
 let value2 = document.querySelector("#value2")
 let value3 = document.querySelector("#value3")
 let value4 = document.querySelector("#plot-content")
+let actorList = document.querySelector('#actor-list')
 
 button.addEventListener('click', async ()=> {
   let input = document.querySelector("#inputBar").value
-  if (input == ``) { input = '66511ac9f5c8f989d5fc20bf' }
+  if (input == ``) { input = 1 }
 
   try {
-    let response = await axios.get(`http://localhost:3001/movies/${input}`);
+    // let response = await axios.get(`http://localhost:3001/movies/${input}`);
+    let response = await axios.get(`http://localhost:3001/movies`);
     console.log(response.data); // Log the data received from the API
     
-    movieName.innerText = response.data.title
-    value1.innerText = response.data.releaseYear
-    value2.innerText = response.data.rating
-    value3.innerText = response.data.runtimeMinutes
-    value4.innerText = response.data.description
+    let singleMovie = response.data[input]
+    movieName.innerText = singleMovie.title
+    value1.innerText = singleMovie.releaseYear
+    value2.innerText = singleMovie.rating
+    value3.innerText = singleMovie.runtimeMinutes
+    value4.innerText = singleMovie.description
 
-    //somehow i need to access an array of actors and reviews from the movie object. but how? if only the child has the parent?
+    // let actorID = singleMovie.actors[0]
+    
+    let actorsIDArray = singleMovie.actors
+
+    const fetchActorDetails = async (actorID) => {
+      try {
+        console.log(actorID);
+        let singleActor = await axios.get(`http://localhost:3001/actors/${actorID}`);
+        const newElement = document.createElement('div');
+        newElement.classList.add('info');
+        newElement.innerText = singleActor.data.name;
+        actorList.appendChild(newElement);
+      } catch (error) {
+        console.error(`Error fetching actor with ID ${actorID}:`, error);
+      }
+    };
+    
+    const loadActors = async () => {
+      for (const actorID of actorsIDArray) {
+        await fetchActorDetails(actorID);
+      }
+    };
+    
+    loadActors();
 
   } catch (error) {
     console.error('Error fetching data:', error);  
